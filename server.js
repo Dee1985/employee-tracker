1c44aca68803a469c657b50869f3524888b9f659
AyDy Burling <aydy.you@gmail.com>
2019-12-19 01:33:52 -0600

    started setting up table. files linked

---
+++ server.js
@@ -0,0 +1,18 @@
+  // Dependencies
+const connection = require("./db/db.js");
+const inquirer = require("inquirer");
+const cTable = require("console.table")
+
+function startQ() {
+
+    function firstQ() {
+
+    inquirer.prompt({
+        type: "list",
+        message: "What would you like to do?",
+        choices: ['View All Employees', 'View All Departments', 'View All Roles', 'Add Employee', 'Add Department', 'Add Role', 'Update Role'],
+        name: "choices"
+    })
+
+    }
+startQ();
